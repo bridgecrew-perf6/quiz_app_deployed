bf01b958a2d3f8c1fb2fa03581965162ffbd10c9
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2020-12-18 17:00:23 -0600

    Updates

---
+++ precache-manifest.558c64e7d88a3eda48385ea4d2d4879f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3457256d6fc760c19b9835408b46b86",
+    "url": "/quiz_app_deployed/index.html"
+  },
+  {
+    "revision": "cec88011ff0b99d1a867",
+    "url": "/quiz_app_deployed/static/css/main.0ed229f6.chunk.css"
+  },
+  {
+    "revision": "3248e1e4b2a92d58d33c",
+    "url": "/quiz_app_deployed/static/js/2.4027eb19.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/quiz_app_deployed/static/js/2.4027eb19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cec88011ff0b99d1a867",
+    "url": "/quiz_app_deployed/static/js/main.58a4f372.chunk.js"
+  },
+  {
+    "revision": "56e93cc71ebaaa25d280",
+    "url": "/quiz_app_deployed/static/js/runtime-main.271b9f3d.js"
+  }
+]);
